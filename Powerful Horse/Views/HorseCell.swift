f7dcc4526ed88a0d32fad719fc5f24ef47ba7611
Jonatan Gonzalez <jonatan.gonzalez@fluxit.com.ar>
2020-05-30 13:44:14 -0300

    Generar estructuras al proyecto y agregar el home

---
+++ Powerful Horse/Views/HorseCell.swift
@@ -0,0 +1,24 @@
+//
+//  HorseCell.swift
+//  Powerful Horse
+//
+//  Created by Jonatan Gonzalez on 30/05/2020.
+//  Copyright © 2020 Jonatan Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class HorseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
