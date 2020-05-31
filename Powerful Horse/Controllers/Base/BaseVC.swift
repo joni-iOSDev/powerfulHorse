bca22188b3334d4ba8e1b741cd1d9b4548d5ea43
Jonatan Gonzalez <jonatan.gonzalez@fluxit.com.ar>
2020-05-31 18:03:14 -0300

    update

---
+++ Powerful Horse/Controllers/Base/BaseVC.swift
@@ -0,0 +1,27 @@
+//
+//  BaseVC.swift
+//  Powerful Horse
+//
+//  Created by Jonatan Gonzalez on 31/05/2020.
+//  Copyright © 2020 Jonatan Gonzalez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+    }
+    
+     func goTo(segue: goToVC) {
+        self.performSegue(withIdentifier: segue.rawValue, sender: self)
+    }
+    
+    enum goToVC: String {
+        case goToDashboard = "goToDashboard"
+        case goToDetailHorse = "goToDetailHorse"
+    }
+}
