8225c1102f1dca150bc8f1f232cb02a61ea60e51
Jonatan <jonatan_gonzalez@me.com>
2020-11-30 12:56:44 -0300

    power

---
+++ Powerful Horse SUI/Powerful Horse SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Powerful Horse SUI
+//
+//  Created by Jonatan Gonzalez on 30/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Powerful Horse SUI/Powerful Horse SUI/Powerful_Horse_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Powerful_Horse_SUIApp.swift
+//  Powerful Horse SUI
+//
+//  Created by Jonatan Gonzalez on 30/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Powerful_Horse_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
